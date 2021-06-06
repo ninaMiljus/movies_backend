ad3ca5d2d0cbc61e5a5481d34cf78dfb6e63e1e4
ninaMiljus <nina.miljus@gmail.com>
2021-06-06 22:50:59 +0200

    JWT i login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    protected $redirectTo = "/home";
+
+    public function authenticate(Request $request) {
+
+        //pokupiti kredemcijale
+
+        $credentials = $request->only(['email', 'password']);
+
+
+
+        try {
+             //pokusamo da se ulogujemo
+            if(! $token = \JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 401);
+            }
+        } catch(JWTException $e) {
+            //ako nismo prosli
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        //sve je proslo dobro i vracamo tokens
+
+        return response()->json(compact('token'));
+
+
+    }
+
+    public function getUser() {
+        return User::All();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +23,9 @@ Route::middleware('api')->get('/movies/{id}', [MovieController::class, 'show']);
 Route::middleware('api')->post('/movies', [MovieController::class, 'store']);
 
 Route::middleware('api')->delete('/movies/{id}', [MovieController::class, 'destroy']);
+
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::middleware('jwt')->get('/getUser', [LoginController::class, 'getUser']);
 
 
